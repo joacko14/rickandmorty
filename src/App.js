cb67b59344ee6c0a5ca375da5fc05461cf659a88
Joaquin Vazquez <joacko14@gmail.com>
2023-09-08 23:23:50 -0600

    ✅create-react-app done!

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my first app using completly ReactJS
+          <br />
+          Please be patient with me, my objective is to implement the rickandmortyapi.com API
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
